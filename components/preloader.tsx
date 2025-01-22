743732abdac07d2aae1b372b30e09eba188ae165
idityaGE <am44910606@gmail.com>
2025-01-22 23:18:59 +0530

    feat(preloader): enhance PreLoader component with completion state and improved animations

--- components/preloader.tsx
+++ components/preloader.tsx
@@ -3,13 +3,16 @@
 import React, { useEffect, useState, useCallback } from 'react';
 import NumberFlow from '@number-flow/react';
 import { motion } from 'framer-motion';
-import { cn } from '@/lib/utils';
 
 const PreLoader = () => {
   const [counter, setCounter] = useState(0);
+  const [iscompeleted, setIscompeleted] = useState(false)
 
   const updateCounter = useCallback(() => {
     if (counter === 100) {
+      setTimeout(() => {
+        setIscompeleted(true)
+      }, 500)
       return;
     }
 
@@ -18,7 +21,7 @@ const PreLoader = () => {
       return next > 100 ? 100 : next;
     });
 
-    const delay = Math.floor(Math.random() * 200) + 50;
+    const delay = Math.floor(Math.random() * 500) + 50;
     setTimeout(updateCounter, delay);
   }, [counter]);
 
@@ -33,20 +36,24 @@ const PreLoader = () => {
       className="fixed top-0 left-0 z-20"
     >
 
-      <div
-        className={cn(
-          "fixed bottom-10 right-10 text-9xl text-white font-mono",
-          counter === 100 ? "opacity-0" : "opacity-100"
-        )}
+      <motion.div
+        initial={{ opacity: 1 }}
+        animate={{ opacity: iscompeleted ? 0 : 1 }}
+        transition={{ duration: 0.2 }}
+        className="fixed bottom-10 right-10 text-9xl text-white font-mono"
       >
         <NumberFlow value={counter} />
-      </div>
+      </motion.div>
 
-      <div className="flex w-full">
+      <motion.div
+        animate={{ y: iscompeleted ? '-100%' : '0%' }}
+        transition={{ duration: 1 }}
+        className="flex w-full"
+      >
         {bars.map((_, index) => (
           <div className='bar w-[10vw] bg-black h-[105vh]' ></div>
         ))}
-      </div>
+      </motion.div>
 
     </div >
   );
